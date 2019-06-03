38b1e34e6eb9dd2b514f2e4bbafae0b0ee45cf91
CimZzz <cimzzz@icloud.com>
2019-06-03 19:48:32 +0800

    init

---
+++ example/quicklibs_example.dart
@@ -0,0 +1,6 @@
+import 'package:quicklibs/quicklibs.dart';
+
+
+void main() {
+	print(TimeFormat.format(DateTime.now(), "yyyy-MM-dd HH:mm:ss"));
+}
---
+++ lib/quicklibs.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library quicklibs;
+
+export 'src/format/timeformat.dart';
+
+// TODO: Export any libraries intended for clients of this package.
